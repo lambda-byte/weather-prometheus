34edc58d7b00b099db77036104ca91f1891ea80b
Lambda <elijah.soule@gmail.com>
2022-05-07 16:08:53 -0700

    Actual work

--- config-example.py
+++ config-example.py
@@ -1,5 +1,10 @@
 # I will do this later
 
-apikey = 'please-change'
+apikey = "please-change"
 
-latitude_value = 'please-change'
+# you can find these with https://www.latlong.net/
+latitude_value = "please-change"
+longitude_value = "please-change"
+
+# Set to metric (Celsius) or imperial (Fahrenheit) or choose standard (I don't know what that outputs)
+unit_value = "please-change"
--- weather.py
+++ weather.py
@@ -3,7 +3,14 @@
 # time to import
 import os
 from flask import Flask, json
+import requests
+from time import time, sleep
 # make flask work
 app = Flask(__name__)
 
 # time to do things
+import config
+
+# json url
+
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude_value + "&lon={lon}&units={units}&appid="
