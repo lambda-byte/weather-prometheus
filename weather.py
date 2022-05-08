5cec8bd5e76e3a208eddd838e5c64d54c3389057
Lambda <elijah.soule@gmail.com>
2022-05-07 22:28:05 -0700

    Change lots of things

--- weather.py
+++ weather.py
@@ -1,26 +1,44 @@
-# main.py
+# weather.py
 
-# time to import
+# time to import tons of stuff for some reason
 from config import *
 import os
 import wget
 from flask import Flask, json
-from time import time, sleep
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
 # make flask work
 app = Flask(__name__)
+
+# make BackgroundScheduler easy
+scheduler = BackgroundScheduler()
 
 # time to do things
 
 # json url
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&units=%s&appid=%s"% (latitude_value, longitude_value, unit_value, apikey)
 
-# download function
+# global json name
+json_name = "weather.json"
 
+# download function
+# this checks if the file exists, and if it does it will delete it to prevent duplicates (why do you do this macOS).
 def json_download():
-      wget.download(url, "weather.json")
+      if os.path.exists(json_name):
+            os.remove(json_name)
+      wget.download(url, json_name)
 
 json_download()
 
-while True:
-      sleep(int(scrape_time_value) - time() % int(scrape_time_value))
-      json_download()
+scheduler.add_job(func=json_download, trigger="interval", seconds=int(scrape_time_value))
+scheduler.start()
+
+# wip route
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
