14990ef641673053df030dda5967b6bd9505d2f7
Lambda <elijah.soule@gmail.com>
2022-05-07 18:08:48 -0700

    Actual work

--- weather.py
+++ weather.py
@@ -12,5 +12,9 @@ app = Flask(__name__)
 # time to do things
 
 # json url
-url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&units=%s&appid=%s"\
-      % (latitude_value, longitude_value, unit_value, apikey)
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&units=%s&appid=%s"% (latitude_value, longitude_value, unit_value, apikey)
+
+# download function
+
+def json_download():
+      wget.download(url, "weather.json")
