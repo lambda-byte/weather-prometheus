863dbe60834c84bebfca7c7d21c01bd04610d227
Lambda <96893300+lambda-byte@users.noreply.github.com>
2022-05-08 17:00:15 -0700

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -6,6 +6,3 @@ longitude_value = "please-change"
 
 # Set to metric (Celsius) or imperial (Fahrenheit) or choose standard (I don't know what that outputs)
 unit_value = "please-change"
-
-# the amount of time it takes to scrape. this is in seconds (3600 = 1 hour)
-scrape_time_value = "please-change"
